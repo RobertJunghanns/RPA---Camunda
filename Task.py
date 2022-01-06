c017bf2c67bdbbb14aa9c8ab18a8f3e993279acd
robert.junghanns <robert.junghanns@campus.tu-berlin.de>
2022-01-06 19:30:36 +0100

    incl. GMail API, remainder e-mail

--- Task.py
+++ Task.py
@@ -1,4 +1,6 @@
+from re import S
 from pyzeebe import ZeebeTaskRouter
+from googleAPI import send_mail
 
 router = ZeebeTaskRouter()
 
@@ -10,3 +12,18 @@ async def decrease_v_days(remaining_v_days: int, requested_v_days: int) -> int:
 @router.task(task_type="restore_v_days")
 async def restore_v_days(remaining_v_days: int, requested_v_days: int) -> int:
     return {"remaining_v_days": remaining_v_days+requested_v_days}
+
+@router.task(task_type="send_remainder_app")
+async def send_remainder_app(sup_e_mail: str, applicant_name: str):
+    to = sup_e_mail
+    subject = "Remainder Vacation Approval"
+    text = f"""
+Hello,
+
+your employee {applicant_name} has submitted a vacation application. Please remember to approve your employee's vacation request.
+
+Thank you!
+    """
+
+    send_mail(to=to, subject=subject, message_text=text)
+    return
---
+++ googleAPI.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+
+import os.path
+from re import sub
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+import base64
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+
+
+def create_message(to, subject, message_text):
+    emailMsg = message_text
+    mimeMessage = MIMEMultipart()
+    mimeMessage['to'] = to
+    mimeMessage['subject'] = subject
+    mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+    raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+
+    return raw_string
+
+def send_mail(to, subject, message_text):
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        # Call the Gmail API
+        service = build('gmail', 'v1', credentials=creds)        
+        raw_string = create_message(to=to, subject=subject, message_text=message_text)
+        message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
+        print('Message Id: %s' % message['id'])
+
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+if __name__ == '__main__':
+    main()
