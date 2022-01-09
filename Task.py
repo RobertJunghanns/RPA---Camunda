1eae43703e0c2c4ce6f1c5dade66dfa5234c85b6
Anastasia Prisacaru <anastasia.prisacaru@student.hu-berlin.de>
2022-01-09 17:33:21 +0100

    extended the email to superviser

--- Task.py
+++ Task.py
@@ -14,13 +14,13 @@ async def restore_v_days(remaining_v_days: int, requested_v_days: int) -> int:
     return {"remaining_v_days": remaining_v_days+requested_v_days}
 
 @router.task(task_type="send_remainder_app")
-async def send_remainder_app(sup_e_mail: str, applicant_name: str):
+async def send_remainder_app(sup_e_mail: str, applicant_name: str, requested_v_days: int, start_date: str):
     to = sup_e_mail
-    subject = "Remainder Vacation Approval"
+    subject = "Reminder Vacation Approval"
     text = f"""
 Hello,
 
-your employee {applicant_name} has submitted a vacation application. Please remember to approve your employee's vacation request.
+your employee {applicant_name} has submitted a vacation application for {requested_v_days} days from {start_date}. Please remember to approve your employee's vacation request.
 
 Thank you!
     """
